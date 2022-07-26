5bff22d5a7ab2fbe3d2c94c1f599ef18bf87e33e
Ravshanbek1006 <sravshanbek1006@gmail.com>
2022-07-26 21:21:44 +0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+
+
+      <h1>Список каналов</h1>
+
+      <div class="filter">
+        <span 
+        v-for="(category , index) in categories" 
+        :key="index"
+        :class="{active : category.velu == activeCategoriy }"
+        @click="activeCategoriy = category.velu"
+        >
+         {{category.text}}
+          </span>
+      </div>
+
+      <input type="text" placeholder="Search" v-model=" searChenal " >
+
+      
+      <div class="row"  v-if="this.filterChannels.length" >
+        <a href="" class="row-item" v-for="(channel, index) in filterChannels" :key="index">   
+          <img class="row-img" :src="channel.icon.url_500x500" :alt="channel.name">
+          <div class="text">{{channel.name}}</div>
+          <div class="badge" v-if="channel.input_quality !== 'sd'  ">{{channel.input_quality}} </div>
+        </a>
+      </div>
+      <div class="loader"  v-else>
+        <img src="@/assets/Spin-1s-200px.svg" alt="">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data(){
+    return{
+      channels: [],
+      searChenal: "",
+      activeCategoriy: "",
+      categories:[
+        {
+          velu: "Все",
+          text: "Все"
+        },
+          {
+          velu: "Новостные",
+          text: "Новостные"
+        },
+          {
+          velu: "Кино",
+          text: "Кино"
+        },
+          {
+          velu: "Премиум",
+          text: "Премиум"
+        },
+          {
+          velu: "Познавательные",
+          text: "Познавательные"
+        },
+          {
+          velu: "Спорт",
+          text: "Спорт"
+        },
+          {
+          velu: "Музыка",
+          text: "Музыка"
+        },
+          {
+          velu: "Детские",
+          text: "Детские"
+        },
+          {
+          velu: "Узбекские",
+          text: "Узбекские"
+        },
+      ]
+
+    }
+  },
+  methods :{
+    getChannels(){
+      fetch('https://api.allplay.uz/api/v1/iptv/channels')
+      .then(res=>{res.json().then(resjson=>{
+        this.channels = resjson.data
+      })
+
+      })
+    }
+  },
+  mounted() {
+    this.getChannels()
+  },
+  computed:{
+    filterChannels(){
+      if(!this.activeCategoriy || this.activeCategoriy=="Все") return this.channels;
+      return this.channels.filter((el)=>{
+       return el.categories.some((cat) => cat.name == this.activeCategoriy);
+      }).filter((el)=> {
+        return el.name.toLowerCase().includes(this.searChenal.toLowerCase())
+      })
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/assets/css/style.css";
 </style>
